e88c0f43fe6e94e8ee50d376b1b91e079e93779d
tnguyen0000 <tomnguyentn246@gmail.com>
2025-04-10 17:57:45 +1000

    initial commit

---
+++ backend/src/main/server/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import (
+    TestView,
+)
+
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('test/', TestView.as_view(), name="test"),
+]
